608ccb13f46c47ba41454532dd80f480264d1c1b
naslausky <eduardonaslausky@hotmail.com>
2023-11-05 16:21:43 -0300

    Remove um break e mantém o while de descida de pedras mais compreensível no décimo sétimo dia.

--- 17/17.py
+++ 17/17.py
@@ -32,7 +32,8 @@ while (indicePedra < objetivoAtual): # Cada iteração desse while é uma pedra 
 	pedraAtual = [list(coordenada) for coordenada in pedras[tipoPedra]] # Lista para poder ir movendo aos poucos.
 	for coordenada in pedraAtual: # Começa 3 acima da maior altura da torre. O mapa "pedras" já tem as coordenadas transladadas de +3:
 		coordenada[1]+= menorAlturaSemPedra
-	while (True): # Vai movendo a pedra para o lado, e pra baixo:
+	daPraDescer = True
+	while (daPraDescer): # Vai movendo a pedra para o lado, e pra baixo:
 		lado = linha[indiceJato % len(linha)] # Caracter indicando direita ou esquerda.
 		indiceJato = (indiceJato + 1) % len(linha)
 		direita = lado == '>'
@@ -49,7 +50,7 @@ while (indicePedra < objetivoAtual): # Cada iteração desse while é uma pedra 
 			if todasAsCoordenadasPodemMover:
 				for coordenada in pedraAtual:
 					coordenada[0] += delta					
-		daPraDescer = True
+		
 		for coordenada in pedraAtual:
 			coordenadaInferior = (coordenada[0], coordenada[1] - 1)
 			if coordenadaInferior in mapa: # Alguma delas não dá pra descer mais.
@@ -57,51 +58,50 @@ while (indicePedra < objetivoAtual): # Cada iteração desse while é uma pedra 
 		if daPraDescer: # Todas as coordenadas podem descer. Descer 1 andar em todas.
 			for coordenada in pedraAtual:
 				coordenada[1] -= 1
-		else: # A pedra estabilizou no fundo.
-			maiorY = 0 # Pode usar o max diretamente com a lambda.
-			for coordenada in pedraAtual: # Adiciona a pedra já estabilizada no mapa.
-				maiorY = max(coordenada[1], maiorY)
-				mapa[tuple(coordenada)] = None
-				oChaoFechou = True # Verificar se o chão fechou uma linha completa:
-				alturaDestaCoordenada = coordenada[1]
-				for x in range(7):
-					if (x, alturaDestaCoordenada) not in mapa:
-						oChaoFechou = False
-				if oChaoFechou: # Atualiza com o potencial novo chão mais alto.
-					alturaDoChaoMaisAlto = max(alturaDoChaoMaisAlto, alturaDestaCoordenada)
-			menorAlturaSemPedra = max(menorAlturaSemPedra, maiorY + 1) # Atualiza com a nova altura máxima da torre.
-			
-			# Limpar o que tiver abaixo do chão mais alto:
-			mapa = {chave: valor for chave, valor in mapa.items() if chave[1]>=alturaDoChaoMaisAlto}
-			
-			numeros = [] # Gera o estado para salvar no gabarito:
-			for indiceColuna in range(7): # Para cada pilastra do mapa.
-				numeroDestaColuna = '' # Gera um número binário representando o estado desta pilastra.
-				for altura in range(alturaDoChaoMaisAlto, menorAlturaSemPedra):
-					numeroDestaColuna += '1' if (indiceColuna, altura) in mapa else '0'
-				numeros.append(int(numeroDestaColuna, 2))
-				
-			numeros.append(indiceJato) # Também faz parte do estado o índice em nosso input,
-			numeros.append(tipoPedra) # e o tipo da pedra que estabilizou.
-			chaveParaSalvarNoGabarito = tuple(numeros)
-			if (chaveParaSalvarNoGabarito) in gabarito: # Este estado repetiu.
-				periodoParaRepetir = indicePedra - gabarito[chaveParaSalvarNoGabarito][0] # Verificar a diferença entre iterações
-				quantoFaltaParaTerminar = objetivoAtual - indicePedra # ... e pular o máximo possível de iterações.
-				if periodoParaRepetir != 0: 
-					quantosPassosPossoPular = quantoFaltaParaTerminar // periodoParaRepetir
-					indicePedra += (quantosPassosPossoPular * periodoParaRepetir)
-					# Atualizar as alturas do mapa com os N pulos de ciclo que fizemos.
-					distanciaQueSubiuEm1Periodo = alturaDoChaoMaisAlto - gabarito[chaveParaSalvarNoGabarito][1]
-					alturaASubir = quantosPassosPossoPular * distanciaQueSubiuEm1Periodo
-					mapa = {(chave[0], chave[1] + alturaASubir) : valor for chave, valor in mapa.items()}
-					alturaDoChaoMaisAlto += alturaASubir
-					menorAlturaSemPedra += alturaASubir			
-			else: # O estado não repetiu, salvar no gabarito para conferir no futuro.
-				gabarito[chaveParaSalvarNoGabarito] = (indicePedra, alturaDoChaoMaisAlto)
-				
-			if (indicePedra == objetivoAtual-1): # Verifica se chegou ao objetivo.
-				print('Altura da torre após', objetivoAtual, 'pedras:', menorAlturaSemPedra)
-				if (objetivoAtual != objetivoSeguinte): # Atualiza para o objetivo da parte 2.
-					objetivoAtual = objetivoSeguinte
-			break # Acabou esta iteração de pedra.
+
+	maiorY = 0 # Pode usar o max diretamente com a lambda.
+	for coordenada in pedraAtual: # Adiciona a pedra já estabilizada no mapa.
+		maiorY = max(coordenada[1], maiorY)
+		mapa[tuple(coordenada)] = None
+		oChaoFechou = True # Verificar se o chão fechou uma linha completa:
+		alturaDestaCoordenada = coordenada[1]
+		for x in range(7):
+			if (x, alturaDestaCoordenada) not in mapa:
+				oChaoFechou = False
+		if oChaoFechou: # Atualiza com o potencial novo chão mais alto.
+			alturaDoChaoMaisAlto = max(alturaDoChaoMaisAlto, alturaDestaCoordenada)
+	menorAlturaSemPedra = max(menorAlturaSemPedra, maiorY + 1) # Atualiza com a nova altura máxima da torre.
+	
+	# Limpar o que tiver abaixo do chão mais alto:
+	mapa = {chave: valor for chave, valor in mapa.items() if chave[1]>=alturaDoChaoMaisAlto}
+	
+	numeros = [] # Gera o estado para salvar no gabarito:
+	for indiceColuna in range(7): # Para cada pilastra do mapa.
+		numeroDestaColuna = '' # Gera um número binário representando o estado desta pilastra.
+		for altura in range(alturaDoChaoMaisAlto, menorAlturaSemPedra):
+			numeroDestaColuna += '1' if (indiceColuna, altura) in mapa else '0'
+		numeros.append(int(numeroDestaColuna, 2))
+		
+	numeros.append(indiceJato) # Também faz parte do estado o índice do nosso input,
+	numeros.append(tipoPedra) # e o tipo da pedra que estabilizou.
+	chaveParaSalvarNoGabarito = tuple(numeros)
+	if (chaveParaSalvarNoGabarito) in gabarito: # Este estado repetiu.
+		periodoParaRepetir = indicePedra - gabarito[chaveParaSalvarNoGabarito][0] # Verificar a diferença entre iterações
+		quantoFaltaParaTerminar = objetivoAtual - indicePedra # ... e pular o máximo possível de iterações.
+		if periodoParaRepetir != 0: 
+			quantosPassosPossoPular = quantoFaltaParaTerminar // periodoParaRepetir
+			indicePedra += (quantosPassosPossoPular * periodoParaRepetir)
+			# Atualizar as alturas do mapa com os N pulos de ciclo que fizemos.
+			distanciaQueSubiuEm1Periodo = alturaDoChaoMaisAlto - gabarito[chaveParaSalvarNoGabarito][1]
+			alturaASubir = quantosPassosPossoPular * distanciaQueSubiuEm1Periodo
+			mapa = {(chave[0], chave[1] + alturaASubir) : valor for chave, valor in mapa.items()}
+			alturaDoChaoMaisAlto += alturaASubir
+			menorAlturaSemPedra += alturaASubir			
+	else: # O estado não repetiu, salvar no gabarito para conferir no futuro.
+		gabarito[chaveParaSalvarNoGabarito] = (indicePedra, alturaDoChaoMaisAlto)
+		
+	if (indicePedra == objetivoAtual-1): # Verifica se chegou ao objetivo.
+		print('Altura da torre após', objetivoAtual, 'pedras:', menorAlturaSemPedra)
+		if (objetivoAtual != objetivoSeguinte): # Atualiza para o objetivo da parte 2.
+			objetivoAtual = objetivoSeguinte
 	indicePedra += 1
